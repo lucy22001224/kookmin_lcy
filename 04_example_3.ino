cc421b9897c73685605eac19d68b9364ccc8272f
lucy22001224 <113411351+lucy22001224@users.noreply.github.com>
2022-09-13 14:36:15 +0900

    Create 04_example_3.ino
    숙제

---
+++ 04_example_3.ino
@@ -0,0 +1,25 @@
+#define PIN_LED 13
+unsigned int count, toggle;
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT)
+  Serial.begin(115200); //Initialize serial port
+  while (!Serial){
+    ;//Wait for serial port to connect.
+  }
+   Serial.println("Hello World");
+   count = toggle = 0;
+   digitalWrite(PIN_LED, toggle); // turn off LED.
+}
+
+void loop() {
+  Serial.println(++count);
+  toggle = toggle_state(toggle); //toggle LED value.
+  difitalWrite(PIN_LED, toggle); // update LED statue.
+  delay(1000);
+}
+
+int toggle_state(int toggle){
+  return toggle;
+}
+}
